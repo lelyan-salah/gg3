44967eb10721d80d5fc8090885d073479c956f51
lelyan salah <salahlelyan@gmail.com>
2023-09-18 13:46:41 +0300

    Initial commit

---
+++ test1.py
@@ -0,0 +1,38 @@
+def add(x=0, y=0):
+    total = x + y
+    return total
+num1 = int(input("Enter number1: "))
+num2 = int(input("Enter number2: "))
+total= add(num1,num2)
+print(total)
+
+def sub(x=0, y=0):
+    total = x - y
+    return total
+num1 = int(input("Enter number1: "))
+num2 = int(input("Enter number2: "))
+total= sub(num1,num2)
+print(total)
+
+
+
+def div(x=0, y=0):
+    total = x / y
+    return total
+
+num1 = int(input("Enter number1: "))
+num2 = int(input("Enter number2: "))
+total= div(num1,num2)
+print(total)
+
+
+def mul(x=0, y=0):
+    total = x * y
+    return total
+num1 = int(input("Enter number1: "))
+num2 = int(input("Enter number2: "))
+total= mul(num1,num2)
+print(total)
+
+
+
